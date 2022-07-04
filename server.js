6e5010c7441558e4960bd52c8445f9efddddf78a
rachelschipull <rlschipull@gmail.com>
2022-07-04 16:01:19 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 3000
+require('dotenv').config()
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'successStats'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/',(request, response)=>{
+    db.collection('stats').find().toArray()
+    .then(data => {
+        response.render('index.ejs', { info: data })
+    })
+    .catch(error => console.error(error))
+})
+
+app.post('/addStat', (request, response) => {
+    db.collection('stats').insertOne({prevTitle: request.body.prevTitle,
+    prevIncome: request.body.prevIncome, techTitle: request.body.techTitle, techIncome: request.body.techIncome})
+    .then(result => {
+        console.log('Stat Added')
+        response.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
